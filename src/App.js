34997836947a207d94f3560189a5ac39aeecf9bb
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-10-30 19:53:09 +0300

    using map in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header  from "./components/Header";
+import Tasks  from "./components/Tasks";
 
 function App() {
   return (
     <div className="container">
 
       <Header />
+      <Tasks />
 
     </div>
   );
