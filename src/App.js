85a474dd60dc142b89d84af772ead42a9839cc5e
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-10-30 11:42:27 +0300

    creating and importing components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Header  from "./components/Header";
 
 function App() {
+  const name = 'mike';
   return (
     <div className="container">
+      <Header />
       <h1>Hello from react</h1>
+      <h4>this is from {name}</h4>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Task Tracker </h1>
+    </div>
+  )
+}
+
+export default Header
