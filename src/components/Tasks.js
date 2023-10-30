f253da962168e9146cc56ba56a50a09d8caab546
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-10-30 20:27:08 +0300

    task component and react icons

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,10 @@
+import Task from "./Task"
 
 const Tasks = ({ tasks }) => {
   return (
     <>
     {tasks.map((task) => (
-      <h3 key={task.id}>{task.text}</h3>
+      <Task key={task.id} task={task} />
     )
     )}
     </>
