75318b9d0cae799abc54860aaf27095058bd50ae
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-10-30 15:38:18 +0300

    proptypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 
 const Header = ({ title }) => {
   return (
@@ -9,6 +10,10 @@ const Header = ({ title }) => {
 
 Header.defaultProps = {
   title: 'Task Tracker',
+}
+
+Header.propTypes = {
+  title: PropTypes.string.isRequired,
 }
 
 export default Header
