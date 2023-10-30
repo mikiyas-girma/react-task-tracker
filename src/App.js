15a6e7841bed6b981a4a52ee0aee3badbad2f5f0
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-10-30 21:17:42 +0300

    delete task and prop drilling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header  from "./components/Header";
 import Tasks  from "./components/Tasks";
 
 function App() {
-
   const [tasks, setTasks] = useState([
     {
       id : 1, 
@@ -25,11 +24,16 @@ function App() {
     },
   ]);
 
+  // delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
   return (
     <div className="container">
 
       <Header />
-      <Tasks tasks={tasks} />
+      <Tasks tasks={tasks} onDelete = {deleteTask} />
 
     </div>
   );
