5b8751a71647f1a92cd02945ace2bc0b524dfb13
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-11-01 20:32:32 +0300

    add task submitted

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,11 +1,27 @@
 import { useState } from "react"
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
   const [text, setText] = useState('');
   const [day, setDay] = useState('');
   const [reminder, setReminder] = useState(false);
+
+  const onSubmit = (e) => {
+    e.preventDefault(); // prevent default submit behavior
+
+    if (!text) {
+      alert('Please add a task');
+      return;
+    }
+
+    onAdd({ text, day, reminder });
+
+    setText('');
+    setDay('');
+    setReminder(false);
+  }
+
   return (
-    <form className="add-form" >
+    <form className="add-form" onSubmit={onSubmit} >
       <div className="form-control">
         <label >Task</label>
         <input
@@ -25,7 +41,9 @@ const AddTask = () => {
       <div className="form-control form-control-check">
         <label >Set Reminder</label>
         <input 
-          type="checkbox" value={reminder}
+          type="checkbox" 
+          checked={reminder}
+          value={reminder}
           onChange={(e)  => setReminder(e.currentTarget.checked)}  
         />
       </div>
