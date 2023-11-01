cebcd1e0c227977b091f20c9b1634e40540b3d21
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-11-01 20:52:18 +0300

    Button toggle for add task form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks  from "./components/Tasks";
 import AddTask from "./components/AddTask";
 
 function App() {
+  const [showAddTask, setShowAddTask] = useState(false);
   const [tasks, setTasks] = useState([
     {
       id : 1, 
@@ -44,8 +45,9 @@ function App() {
 
   return (
     <div className="container">
-      <Header />
-      <AddTask onAdd = {addTask} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} 
+      showAdd={showAddTask} />
+      {showAddTask && <AddTask onAdd = {addTask} />}
       { tasks.length > 0 ? ( <Tasks tasks={tasks} onDelete = {deleteTask} onToggle = {toggleReminder}/> ): ( 'no tasks to display' ) 
       }
 
