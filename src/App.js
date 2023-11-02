6d75cdd4cbf60179f33c05f5eeb795671d843d16
Mikiyas-Girma <mikiyasgirmaET@gmail.com>
2023-11-02 19:45:31 +0300

    Add task to the server

--- src/App.js
+++ src/App.js
@@ -20,18 +20,33 @@ function App() {
     const res = await fetch('http://localhost:5000/tasks');
     const data = await res.json();
 
+    console.log(data)
     return data;
   }
 
   // Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = {id , ...task};
-    setTasks([...tasks, newTask]);
+  const addTask = async (task) => {
+    const res = await fetch(`http://Localhost:5000/tasks`, {
+      method: 'POST',
+      headers: {
+        'Content-type' : 'application/json'
+      },
+      body: JSON.stringify(task)
+    })
+
+    const data = await res.json();
+    setTasks([...tasks, data]);
+
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = {id , ...task};
+    // setTasks([...tasks, newTask]);
   }
 
   // delete task
-  const deleteTask = (id) => {
+  const deleteTask = async (id) => {
+    await fetch(`http://localhost:5000/tasks/${id}`, 
+    {method: 'DELETE'});
+
     setTasks(tasks.filter((task) => task.id !== id));
   }
 
